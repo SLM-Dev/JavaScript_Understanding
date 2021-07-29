6c043fb61e385dc3bfa725e7cb354beac2e23885
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-07-29 16:24:12 +0000

    wirk on later

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Hi we just loaded app.js')
+console.log(2+2);
+
+
+
+function printName(first, last) {
+  console.log(first + ' ' + last);
+  alert(first + ' ' + last);
+
+
+
+
+  
+
+    
+};
